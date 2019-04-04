7e0c67de42c7f263bc606f2bcdad9bcec3b879ad
josh671 <jmb671@hotmail.com>
2019-04-03 18:58:32 -0700

    commit message

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const http = require("http");  
+const http = require("http"); 
 http.createServer((req,res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('hafa adai');
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+     // res.writeHead(200, {'Content-Type': 'text/plain'});
+     // res.end('home.html');
+     const fs = require("fs");
+     fs.readFile("public/home.html", (err, data) => {
+       if (err) return console.error(err);
+       res.writeHead(200, {'Content-Type': 'text/html'});
+       res.end(data.toString());
+    });
+
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('about.html');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
 }).listen(process.env.PORT || 3000);
