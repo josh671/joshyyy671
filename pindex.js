084c8b0383819907d3db8fe67ca4565a5925e747
josh671 <jmb671@hotmail.com>
2019-05-20 18:46:52 -0700

    all methods work, cant get array ammount to post on delete page

--- pindex.js
+++ pindex.js
@@ -44,6 +44,7 @@ app.get('/getAll',(req,res)=>{
  //GET() post method with mongodb/////////////////////////GET()///////////////////////////////////////////////////////////
 app.get('/get', (req,res,next)=>{
   console.log(req.body.inventorFirst);
+  
   inventDB.findOne({'first':req.body.inventorFirst} ,(err,found)=>{ 
     if(err) return next(err); 
     res.type('text/html');
@@ -54,7 +55,7 @@ app.get('/get', (req,res,next)=>{
 
  app.post('/detail', (req, res, next)=> { 
   inventDB.findOne({'first' :req.body.inventorFirst}).then((found)=>{
-    console.log(found); 
+    //console.log(found); 
     res.render("detail", { result:found});
   }).catch((err) =>{ 
     return next (err);
@@ -72,8 +73,10 @@ app.get('/get', (req,res,next)=>{
 //DELETE METHOD////////////DELETE METHOD//////////
 app.get('/delete', (req, res, ) => { 
 inventDB.deleteOne({'first': req.query.first}).then((result)=>{
-  //console.log(result);
-  res.render("delete", {first: req.query.first, deleted: result})
+  
+  ;
+  res.render("delete", {first: req.query.first, deleted:result})
+  
 })
 });
   //result.deleteCoundresult.deleteCount undefined;
@@ -86,6 +89,22 @@ inventDB.deleteOne({'first': req.query.first}).then((result)=>{
  // console.log(result); 
   res.render('delete', {title: req.body.first, result: result});
 }); */
+
+
+app.post('/add', (req,res) => { 
+  //console.log(req.body.addItem);
+let newItem = {'first': req.body.addItem}; 
+
+inventDB.updateOne({'first':req.body.addItem}, newItem, {upsert:true}, (err, result)=>{ 
+  if(err) return next (err);
+  //console.log (result);
+  res.type('text/html')
+  res.render("" , {result:result});
+})
+
+}); 
+
+
 
 
    // send plain text response
